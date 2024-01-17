8c53fd2cd4fe75be0858314bb172c9347449bf81
Joel Alves Pereira <atejap05@yahoo.com.br>
2024-01-17 11:14:12 -0300

    ajusta floating btn

--- src/components/ui/FloatingButton.tsx
+++ src/components/ui/FloatingButton.tsx
@@ -29,7 +29,7 @@ const FloatingButton = () => {
   };
 
   return (
-    <div className="floating-btn hidden fixed bottom-14 right-32 shadow-2xl">
+    <div className="floating-btn hidden fixed bottom-14 right-12 sm:right-32 shadow-2xl">
       <button
         onClick={scrollToTop}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
